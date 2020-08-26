44647581f119e6373c1ec59b56d629665687651d
wrms <wrms@cin.ufpe.br>
2020-08-26 15:14:10 -0300

    Documentação monitor

--- mape.py
+++ mape.py
@@ -1,9 +1,8 @@
-from monitor import coletar_metrica_prometheus, coletar_numero_replicas
+from monitor import coletar_dados_prometheus, coletar_numero_replicas
 
-load_time = coletar_metrica_prometheus('LoadTime', '[5m:1m]')
-traffic = coletar_metrica_prometheus('Traffic', '[5m:1m]')
+load_time = coletar_dados_prometheus('LoadTime', '[5m:1m]')
+traffic = coletar_dados_prometheus('Traffic', '[5m:1m]')
 pods = coletar_numero_replicas('default')
-
 
 print('Métrica LoadTime')
 for key, value in load_time.items():
@@ -19,15 +18,3 @@ print('Número de pods')
 for key, value in pods.items():
     print(key, value)
 print()
-
-
-"""
-Load Time
-Traffic
-Numero de conteineres
-
-Recency *
-Number of Logins *
-Stability *
-Number of Logins *
-"""
--- monitor.py
+++ monitor.py
@@ -8,23 +8,103 @@ METRICAS = {
         'destination_workload_namespace=~"default"}[1m])) by (destination_workload)'}
 
 
-def coletar_metrica_prometheus(metrica, range):
-    request_prometheus_api = criar_requisicao_prometheus(metrica, range)
-    dados = requisitar_prometheus(request_prometheus_api)
+def coletar_dados_prometheus(metrica, range=''):
+    """Coletar quaisquer métricas do Prometheus
+        Args:
+            metrica (str): Chave da métrica do Prometheus a ser consultada
+            no dicionário METRICAS.
+
+            range (str): intervalo de informações que será retornado.
+
+        Returns:
+            dados_processados (dict):
+
+            Se a variável range for vazia, o usuário receberá um
+            dicionário com todos os deployments e o seus valores
+            no instante atual.
+
+            Por exemplo: {adservice: 100,
+                          frontned: 50}
+
+            Se a variável range for especificado, o usuário receberá um
+            dicionário com todos os deployments e os valores da métrica
+            conforme sua especificação.
+
+            Por exemplo: Se range = ['2m:1m']
+                         {adservice: [100, 102],
+                          frontend: [50, 100]}
+
+            Por exemplo: Se range = ['4m:1m']
+                         {adservice: [100, 102, 109, 112],
+                          frontend: [50, 100, 115, 125]}
+    """
+    requisicao_prometheus_api = criar_requisicao_prometheus(metrica, range)
+    dados = requisitar_prometheus(requisicao_prometheus_api)
     dados_processados = processar_dados(dados)
 
     return dados_processados
 
 
-def criar_requisicao_prometheus(metrica, intervalo_tempo):
-    return PROMETHEUS_URL + METRICAS.get(metrica) + intervalo_tempo
+def criar_requisicao_prometheus(metrica, range):
+    """Constrói a requisição do Prometheus
+        Args:
+            metrica (str): Chave da métrica do Prometheus a ser consultada
+            no dicionário METRICAS.
+
+            range (str): intervalo de informações que será retornado.
+
+        Returns:
+            A URL de consulta ao Prometheus para uma determinada métrica (str)
+    """
+    return PROMETHEUS_URL + METRICAS.get(metrica) + range
 
 
 def requisitar_prometheus(request_prometheus_api):
+    """Coletar dados no prometheus através de um GET.
+
+        Args:
+            request_prometheus_api (str): A URL de consulta ao Prometheus
+            para uma determinada métrica.
+
+        Returns:
+            Dado bruto do Prometheus seguindo a estruturação do json (str).
+    """
     from json import loads
     from requests import get
 
-    dados = loads(get(request_prometheus_api).text)
+    return loads(get(request_prometheus_api).text)
+
+
+def processar_dados(dados):
+    """Processar e tratar os dados coletados no Prometheus.
+        Args:
+            dados (str): Dados brutos de um determina consulta ao Prometheus.
+
+        Returns:
+            Informações processadas e tratadas (dict).
+
+            Se a variável range for vazia, o usuário receberá um
+            dicionário com todos os deployments e o seus valores
+            no instante atual.
+
+            Por exemplo: {adservice: 100,
+                          frontned: 50}
+
+            Se a variável range for especificado, o usuário receberá um
+            dicionário com todos os deployments e os valores da métrica
+            conforme sua especificação.
+
+            Por exemplo: Se range = ['2m:1m']
+                         {adservice: [100, 102],
+                          frontend: [50, 100]}
+
+            Por exemplo: Se range = ['4m:1m']
+                         {adservice: [100, 102, 109, 112],
+                          frontend: [50, 100, 115, 125]}
+    """
+    from numpy import array
+
+    # Caso o usuário tenha informado algum range (Processar N respostas).
     if dados['data']['result'][0].get('values') is not None:
         dict_requests = {}
         for index in dados['data']['result']:
@@ -32,8 +112,7 @@ def requisitar_prometheus(request_prometheus_api):
                 dict_requests[index['metric']['destination_workload']] = index['values']
             except KeyError:
                 dict_requests[index['metric']['workload']] = index['values']
-
-        return dict_requests
+    # Caso o usuário não tenha informado nenhum range (Processar 1 resposta).
     else:
         dict_requests = {}
         for index in dados['data']['result']:
@@ -42,51 +121,32 @@ def requisitar_prometheus(request_prometheus_api):
             except KeyError:
                 dict_requests[index['metric']['workload']] = index['value']
 
-        return dict_requests
-
-
-def processar_dados(dados):
-    from numpy import array
-    for key, value in dados.items():
+    # Removendo dados inúteis do dicionário, p. e.x., tempo da métrica.
+    dados_processados = {}
+    for key, value in dict_requests.items():
         if array(value).ndim == 1:
-            dados[key] = float(value[1])
+            dados_processados[key] = float(value[1])
         else:
             aux_list = []
             for index in value:
                 aux_list.append(float(index[1]))
-            dados[key] = aux_list
-    return dados
+            dados_processados[key] = aux_list
+    return dados_processados
 
 
 def coletar_numero_replicas(namespace):
-    dict_namespace = coletar_deployments_from_kubernetes_namespace(namespace)
+    """API do Kubernetes responsável por coletar o número de réplicas
+        de um Deployment implantando no Kubernetes.
 
-    for key, value in dict_namespace.items():
-        dict_namespace[key] = coletar_replicas_kubernetes(key, 'default')
+        Args:
+            namespace (str): nome do namespace do Kubernetes.
 
-    return dict_namespace
+        Returns:
+            Dicionário com a quantidade de pods por deployment (dict).
 
-
-def coletar_replicas_kubernetes(nome_deployment, namespace):
-    from kubernetes.client.rest import ApiException
-    from kubernetes import config, client
-
-    configuration = config.load_kube_config()
-
-    # create an instance of the API class
-    api_instance = client.AppsV1Api(client.ApiClient(configuration))
-    name = nome_deployment  # str | name of the Deployment
-    namespace = namespace  # str | object name and auth scope, such as for teams and projects
-
-    try:
-        api_response = api_instance.read_namespaced_deployment_scale(name, namespace)
-        return api_response.spec.replicas
-
-    except ApiException as e:
-        print("Exception when calling AppsV1Api->read_namespaced_deployment_scale: %s\n" % e)
-
-
-def coletar_deployments_from_kubernetes_namespace(namespace):
+            Por exemplo: {adservice: 1,
+                          frontned: 2}
+    """
     from kubernetes.client.rest import ApiException
     from kubernetes import config, client
 
@@ -99,7 +159,10 @@ def coletar_deployments_from_kubernetes_namespace(namespace):
         api_response = api_instance.list_namespaced_deployment(namespace=namespace)
         dict_namespace = {}
         for i in range(0, len(api_response.items)):
-            dict_namespace[api_response.items[i].metadata.name] = 0
+            # dict_namespace[api_response.items[i].metadata.name] = 0
+            deployment = api_response.items[i].metadata.name
+            dict_namespace[deployment] = api_instance.read_namespaced_deployment_scale(deployment,
+                                                                                       namespace).spec.replicas
         return dict_namespace
     except ApiException as e:
         print("Exception when calling AppsV1beta1Api->list_namespaced_deployment: %s\n" % e)
